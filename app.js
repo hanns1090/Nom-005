ac079441cadfacc0dfcd7cce551e51422e651657
Hanns <hanns1090@gmail.com>
2020-07-31 13:59:53 -0500

    Primer commit con SSH

---
+++ app.js
@@ -0,0 +1,14 @@
+
+function calcularFunction(){
+
+   var in1 =document.getElementById('total')
+   var in2 =document.getElementById('total')
+   
+   var res = parseFloat(in1) + parseFloat(in2)
+   document.getElementById('resultado').textContent = res
+}
+
+var function handsome(v) {
+   this.valor = v
+   
+}
